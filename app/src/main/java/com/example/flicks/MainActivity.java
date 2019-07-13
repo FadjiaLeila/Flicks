40dbbbb04201f58c00f6f868ea539c40ad3d48d6
FadjiaLeila <fadjia20@gmail.com>
2019-07-13 08:57:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flicks/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.flicks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+
+import com.example.flicks.adapters.MoviesAdapter;
+import com.example.flicks.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String MOVIE_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+ List<Movie> movies;
+    //Add RecyclerView support library to the Gradle build file fait
+    //Define a model class to use as the data source fait
+          //  Add a RecyclerView to your activity to display the items fait
+          //  Create a custom row layout XML file to visualize the item fait
+         //   Create a RecyclerView. fait
+   // Adapter and ViewHolder to render the item fait
+   // Bind the adapter to the data source to populate the RecyclerView
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       RecyclerView rvMovies = findViewById(R.id.rvMovies);
+       movies = new ArrayList<>();
+        final MoviesAdapter adapter = new MoviesAdapter(this, movies);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        rvMovies.setAdapter(adapter);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(MOVIE_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray movieJsonArray = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray));
+
+                    Log.d("smile", movies.toString());
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/flicks/adapters/MoviesAdapter.java
@@ -0,0 +1,86 @@
+package com.example.flicks.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.flicks.R;
+import com.example.flicks.models.Movie;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+    Context context ;
+    List<Movie> movies;
+
+    public MoviesAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("smile", "OnCreateViewHolder");
+       View view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("smile", "OnBindViewHolder" + position);
+      Movie movie =  movies.get(position);
+    holder.bind(movie);
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+           tvTitle = itemView.findViewById(R.id.tvTitle);
+           tvOverview = itemView.findViewById(R.id.tvOverview);
+           ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageURL = movie.getPosterPath();
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageURL = movie.getBackdropPath();
+            }
+            Glide.with(context).load(imageURL).apply(new RequestOptions().placeholder(R.drawable.download)).into(ivPoster);
+        }
+
+
+
+
+
+    }
+}
